9463f4d5e5512352ac1868a30522b7d183047a43
Sam Marshall <sjmarshy@gmail.com>
2018-03-31 12:22:28 +0100

    feat: better title

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ import { h, app } from 'hyperapp';
 
 const state = {
     seeds: 1,
-    plants: 0
-}
+    plants: 0,
+};
 
 const actions = {
-    plant: value => state => ({ seeds: state.seeds - 1, plants: state.plants + 1 })
-}
+    plant: value => state => ({
+        seeds: state.seeds - 1,
+        plants: state.plants + 1,
+    }),
+};
 
-const view = (s, a) => (
-    h("div", {}, h("h1", {}, `Outstanding: A Tale of ${s.seeds} Seeds`))
-)
+const view = (s, a) =>
+    h(
+        'div',
+        {},
+        h(
+            'h1',
+            {},
+            `Outstanding: A Tale of ${
+                s.seeds === 1 ? 'a Seed' : `${s.seeds} Seeds`
+            }`
+        )
+    );
 
 app(state, actions, view, document.body);
