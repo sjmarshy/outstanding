8cc53600c18e00d19c3f71806d64090529207b52
Sam Marshall <sjmarshy@gmail.com>
2018-03-31 12:20:49 +0100

    feat: a header

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+import { h, app } from 'hyperapp';
+
+const state = {
+    seeds: 1,
+    plants: 0
+}
+
+const actions = {
+    plant: value => state => ({ seeds: state.seeds - 1, plants: state.plants + 1 })
+}
+
+const view = (s, a) => (
+    h("div", {}, h("h1", {}, `Outstanding: A Tale of ${s.seeds} Seeds`))
+)
+
+app(state, actions, view, document.body);
